3a2bbc4d6eea3a934ae6f185c000f603d4fa13dc
msarbak <m.sarbak@hotmail.com>
2024-04-10 23:05:57 +0300

    project upload

---
+++ src/snake/SnakeGame.java
@@ -0,0 +1,15 @@
+package snake; 
+
+import javax.swing.JFrame;
+
+public class SnakeGame extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		
+		//GameFrame frame = new GameFrame();
+		new GameFrame();
+	}
+
+}
